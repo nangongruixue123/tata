7a98c29df61cd1e08699b2805f360fb20873326b
nangongruixue123 <nangongruixue123@users.noreply.github.com>
2017-10-26 21:34:42 +0800

    add commodity
    add commodity class

---
+++ src/main/java/com/example/demo/cn/soft/com/model/commodity.java
@@ -0,0 +1,11 @@
+package com.example.demo.cn.soft.com.model;
+
+import java.math.BigDecimal;
+
+public class commodity {
+    private String name;
+
+    private String commodityId;
+
+    private BigDecimal commodityPrice;
+}
